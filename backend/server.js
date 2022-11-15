964ee9d578b7542aede736d8325e29faaf1317eb
techshomez <danieltopeg>
2022-11-15 12:44:49 +0100

    Working on user authentication

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,18 @@ const cors = require("cors");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Route working");
+});
 
 // MongoDB Connection
-
+const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI).then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
